12bcd6fa85e69bbca3843d07100af5b33e7e13a8
David Perez <edwinperez539@gmail.com>
2023-02-08 06:28:27 -0500

    Fixing linux config

--- linux/scripts/node.sh
+++ linux/scripts/node.sh
@@ -6,11 +6,7 @@ source _utils.sh
 e_pending "Configuring node"
 # ------------------------------------------------------------------------------
 
-if !has_command "node"; then
-  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-
-  if has_command "npm"; then
-    sed 's/#.*//' files/npm-packages.txt | xargs npm install -g
-  fi
+if has_command "npm"; then
+  sed 's/#.*//' files/npm-packages.txt | xargs npm install -g
 fi
 
