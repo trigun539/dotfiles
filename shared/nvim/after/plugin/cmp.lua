b0b974017ede09709b267345930b40c5bf19109c
David Perez <edwinperez539@gmail.com>
2023-04-30 07:12:17 -0400

    Fixing copilot and cmp tab conflict

--- shared/nvim/after/plugin/cmp.lua
+++ shared/nvim/after/plugin/cmp.lua
@@ -25,32 +25,6 @@ cmp.setup({
       c = cmp.mapping.close(),
     }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    -- ['<Tab>'] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-    ["<Tab>"] = function(fallback)
-      local suggestion = require("copilot.suggestion")
-      print("suggestion.is_visible()", suggestion.is_visible())
-
-      if suggestion.is_visible() then
-        suggestion.accept()
-      elseif cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
