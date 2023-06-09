a8ef3e4c3dcd53d559273181db72c17868acb0a1
Edwin Perez <edwinperez539@gmail.com>
2023-06-09 06:24:20 -0400

    Fixing lsp

--- shared/nvim/after/plugin/lsp.lua
+++ shared/nvim/after/plugin/lsp.lua
@@ -1,65 +1,104 @@
-local lsp = require('lsp-zero')
-local cmp = require('cmp')
+local lsp = require("lsp-zero")
+local cmp = require("cmp")
 
-lsp.preset('recommended')
+lsp.preset("recommended")
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'pylsp',
-  'cssls',
-  'prismals',
-  'dockerls',
-  'bashls',
-  'jsonls',
-  'tailwindcss',
-  'cssmodules_ls',
-  'graphql',
-  'html',
-  'marksman',
-  'ruby_ls',
-  'sqlls',
-  'terraformls',
+	"tsserver",
+	"eslint",
+	"pylsp",
+	"cssls",
+	"prismals",
+	"dockerls",
+	"bashls",
+	"jsonls",
+	"tailwindcss",
+	"cssmodules_ls",
+	"graphql",
+	"html",
+	"marksman",
+	"ruby_ls",
+	"sqlls",
+	"terraformls",
+	"lua_ls",
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {  "vim", "nnoremap", "packer_bootstrap"  }
-            }
-        }
-    }
+lsp.configure("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim", "nnoremap", "packer_bootstrap" },
+			},
+		},
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
 lsp.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = {
-    -- ['lua_ls'] = {'lua'},
-    -- ['rust_analyzer'] = {'rust'},
-    -- ['tsserver'] = {'typescript', 'javascript', 'typescriptreact'},
-    -- if you have a working setup with null-ls
-    -- you can specify filetypes it can format.
-    ['null-ls'] = {'javascript', 'typescript', 'typescriptreact', 'lua', 'rust', 'css', 'html', 'json', 'yaml', 'markdown', 'python', 'dockerfile', 'graphql', 'bash', 'ruby', 'sql', 'terraform'},
-  }
+	format_opts = {
+		async = false,
+		timeout_ms = 10000,
+	},
+	servers = {
+		-- ['lua_ls'] = {'lua'},
+		-- ['rust_analyzer'] = {'rust'},
+		-- ['tsserver'] = {'typescript', 'javascript', 'typescriptreact'},
+		-- if you have a working setup with null-ls
+		-- you can specify filetypes it can format.
+		["null-ls"] = {
+			"javascript",
+			"typescript",
+			"typescriptreact",
+			"lua",
+			"rust",
+			"css",
+			"html",
+			"json",
+			"yaml",
+			"markdown",
+			"python",
+			"dockerfile",
+			"graphql",
+			"bash",
+			"ruby",
+			"sql",
+			"terraform",
+		},
+	},
 })
 
 lsp.nvim_workspace()
