5b5df4fdba9eddb62978158957fa1cf31614a755
David <edwinperez539@gmail.com>
2025-08-06 23:20:19 -0400

    Adding omarchy settings

---
+++ omarchy/base.sh
@@ -0,0 +1,62 @@
+############################################################################
+# PATH
+############################################################################
+
+GEM_HOME=$HOME/.gem
+GEM_PATH=$HOME/.gem
+
+export PATH="/usr/bin:$PATH"
+export PATH="/usr/sbin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="/opt/X11/bin:$PATH"
+export PATH="$PATH:$(go env GOBIN):$(go env GOPATH)/bin"
+
+############################################################################
+# VIM FZF
+############################################################################
+
+export FZF_DEFAULT_COMMAND='ag --nocolor --ignore node_modules -g ""'
+
+############################################################################
+# NVM
+############################################################################
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+############################################################################
+# SDKMAN
+############################################################################
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+############################################################################
+# Z
+############################################################################
+
+source /usr/share/z/z.sh
+
+############################################################################
+# RUBY
+############################################################################
+
+eval "$(rbenv init - zsh)"
+
+############################################################################
+# PYENV
+############################################################################
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
+
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
+eval "$(pyenv init -)"
+
+############################################################################
+# GIT
+############################################################################
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+DISABLE_UNTRACKED_FILES_DIRTY="true"
---
+++ omarchy/utils.sh
@@ -0,0 +1,159 @@
+# ------------------------------------
+# GENERAL
+# ------------------------------------
+
+if [ eza ];
+then
+  alias ll='eza -l --icons'
+else
+  alias ll='ls -alf'
+fi
+
+alias lla='ll -a'
+alias fhere="find . -name "
+alias myip="curl http://ipecho.net/plain; echo"
+
+export REACT_EDITOR=nvim
+
+# ------------------------------------
+# TMUX
+# ------------------------------------
+
+# alias tmux="TERM=screen-256color-bce tmux"
+alias tll="tmux list-sessions"
+ts() { tmux new -s $1; }
+ta() { tmux a -t $1; }
+
+# ------------------------------------
+# TMUXP
+# ------------------------------------
+
+export DISABLE_AUTO_TITLE='true'
+tpl() { tmuxp load $1; }
+
+# ------------------------------------
+# Docker alias and function
+# ------------------------------------
+
+# Start Docker Daemon
+alias dockstart="bash --login '/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh'"
+
+# Get latest container ID
+alias dl="docker ps -l -q"
+
+# Get container process
+alias dps='docker ps --format "table {{.Names}}\t{{.State}}\t{{.Ports}}"'
+
+# Get process included stop container
+alias dpa="docker ps -a"
+
+# Get images
+alias di="docker images"
+
+# Get volume list
+alias dvls="docker volume ls"
+
+# Get volume remove
+alias dvrm="docker volume rm"
+
+# Get container IP
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+
+# Run deamonized container, e.g., $dkd base /bin/echo hello
+alias dkd="docker run -d -P"
+
+# Run interactive container, e.g., $dki base /bin/bash
+alias dki="docker run -i -t -P"
+
+# Execute interactive container, e.g., $dex base /bin/bash
+alias dex="docker exec -i -t"
+
+# Stop all containers
+dstop() { docker stop $(docker ps -a -q); }
+
+# Remove all containers
+drm() { docker rm $(docker ps -a -q); }
+
+# Stop and Remove all containers
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+# Remove image
+dri() { docker rmi $1; }
+
+drif() { docker rmi -f $1; }
+
+# Remove all images
+driall() { docker rmi $(docker images -q --filter "dangling=true"); }
+
+# Dockerfile build, e.g., $dbu tcnksm/test
+dbu() { docker build -t=$1 .; }
+
+# Show all alias related docker
+dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
+
+# ------------------------------------
+# DOCKER COMPOSE
+# ------------------------------------
+
+dcup() { docker-compose up $1; }
+dcpull() { docker-compose pull $1; }
+dcstop() { docker-compose stop $1; }
+dcdown() { docker-compose down $1; }
+dcbuild() { docker-compose build $1; }
+dclogs() { docker-compose logs -f $1; }
+
+# ------------------------------------
+# TERRAFORM
+# ------------------------------------
+
+alias tfi="terraform init"
+alias tfc="terraform console"
+alias tff="terraform fmt"
+alias tfv="terraform validate"
+alias tfp="terraform plan"
+alias tfa="terraform apply"
+alias tfaa="terraform apply --auto-approve"
+alias tfd="terraform destroy"
+alias tfda="terraform destroy --auto-approve"
+
+# ------------------------------------
+# ANSIBLE
+# ------------------------------------
+
+# Run playbook
+arb() { ansible-playbook $1 -i "$HOME/environments/personal/inventory" --extra-vars="target=$2" -K; }
+arv() { ansible-playbook --ask-vault-pass $1 -i "$HOME/environments/personal/inventory" --extra-vars="target=$2" -K; }
+
+# Ansible vault
+avc() { ansible-vault create $1}
+ave() { ansible-vault edit $1}
+aven() { ansible-vault encrypt $1}
+avde() { ansible-vault decrypt $1}
+
+# ------------------------------------
+# AWS
+# ------------------------------------
+
+# Switch aws default profiles
+awsll() {
+  aws configure list-profiles
+}
+awscp() {
+  gsed -i "s/AWS_PROFILE=$(awsp)/AWS_PROFILE=$1/g" ~/dotfiles/local.zsh
+  source ~/dotfiles/local.zsh
+}
+awsp() {
+  echo "$AWS_PROFILE"
+}
+
+# Make private s3 bucket
+awsmbp() { aws s3api create-bucket --bucket $1 --region us-east-1 --acl private; }
+awsS3Block() { aws s3api put-public-access-block \
+    --bucket $1 \
+    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"}
+
+# ------------------------------------
+# Task utils
+# ------------------------------------
+
+alias tskui="taskwarrior-tui"
